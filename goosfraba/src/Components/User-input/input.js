48daa5c1ca3c88999cded5e73aedb48aa82f00e8
LouisWright <louis--wright@hotmail.co.uk>
2022-11-22 19:51:46 +0000

    added functionality for buttons and also user input adds to list

--- goosfraba/src/Components/User-input/input.js
+++ goosfraba/src/Components/User-input/input.js
@@ -1,18 +1,10 @@
-import React from 'react'
-import Todo from './Todo';
+// import React from "react";
+// import Todo from "./Todo";
 
+// function Input({ todos }) {
+//   return todos.map((todo) => {
+//     return <Todo key={todo.to_do_id} todo={todo} />;
+//   });
+// }
 
-
-function Input({ todos }){
-    return (
-        todos.map(todo => {
-            return <Todo key={todo.to_do_id} todo={todo} />
-        })
-        
-    )
-}
-
-
-
-
-export default Input
+// export default Input;
