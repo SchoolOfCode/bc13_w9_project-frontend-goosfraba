c3e13bbc502f3b137bee8c4f285f14a30c45258c
violetacf <violeta@cique.es>
2022-11-30 12:48:26 +0100

    Tidying up

--- goosfraba/src/Components/Links/links.js
+++ goosfraba/src/Components/Links/links.js
@@ -1,38 +1,89 @@
-import "./links.css";
+import './links.css';
 
-
-/**
+/** Links Docs
  * Imports needed: CSS (style)
  * This function is the footer, where the quick links are
  * @returns The seven images with their links, open in new tab when clicked (React, w3Schools, CodeWars, npm, FreeCodeCamp, GitHub, SoC)
  */
 function Links() {
-
   return (
     <footer className="footer">
-        {/* <p className="links"> */}
-        <a data-testid="react-link" target="_blank" rel="noreferrer" href="https://reactjs.org/">
-            <img className="links-logo" src="/Pictures/react-logo.png" alt="react"/>
-        </a>
-        <a data-testid="w3-link" target="_blank" rel="noreferrer" href="https://www.w3schools.com/">
-            <img className="links-logo" src="/Pictures/w3schools-logo.png" alt="w3schools"/>
-        </a>
-        <a data-testid="codeWars-link" target="_blank" rel="noreferrer" href="https://www.codewars.com/dashboard">
-            <img className="links-logo" src="/Pictures/codewars-logo.png" alt="codewars"/>
-        </a>
-        <a data-testid="npm-link" target="_blank" rel="noreferrer" href="https://www.npmjs.com/">
-            <img className="links-logo" src="/Pictures/npm-logo.png" alt="npm"/>
-        </a>
-        <a data-testid="freeCodeCamp-link" target="_blank" rel="noreferrer" href="https://www.freecodecamp.org/">
-            <img className="links-logo" src="/Pictures/freecodecamp-logo.png" alt="freecodecamp"/>
-        </a>
-        <a data-testid="gitHub-link" target="_blank" rel="noreferrer" href="https://github.com/">
-            <img className="links-logo" src="/Pictures/github-logo.png" alt="github"/>
-        </a>
-        <a data-testid="SoC-link" target="_blank" rel="noreferrer" href="https://www.schoolofcode.com/">
-            <img className="links-logo" src="/Pictures/soc-logo.png" alt="soc"/>
-        </a>
-        {/* </p> */}
+      <a
+        data-testid="react-link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://reactjs.org/"
+      >
+        <img
+          className="links-logo"
+          src="/Pictures/react-logo.png"
+          alt="react"
+        />
+      </a>
+      <a
+        data-testid="w3-link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.w3schools.com/"
+      >
+        <img
+          className="links-logo"
+          src="/Pictures/w3schools-logo.png"
+          alt="w3schools"
+        />
+      </a>
+      <a
+        data-testid="codeWars-link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.codewars.com/dashboard"
+      >
+        <img
+          className="links-logo"
+          src="/Pictures/codewars-logo.png"
+          alt="codewars"
+        />
+      </a>
+      <a
+        data-testid="npm-link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.npmjs.com/"
+      >
+        <img className="links-logo" src="/Pictures/npm-logo.png" alt="npm" />
+      </a>
+      <a
+        data-testid="freeCodeCamp-link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.freecodecamp.org/"
+      >
+        <img
+          className="links-logo"
+          src="/Pictures/freecodecamp-logo.png"
+          alt="freecodecamp"
+        />
+      </a>
+      <a
+        data-testid="gitHub-link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/"
+      >
+        <img
+          className="links-logo"
+          src="/Pictures/github-logo.png"
+          alt="github"
+        />
+      </a>
+      <a
+        data-testid="SoC-link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.schoolofcode.com/"
+      >
+        <img className="links-logo" src="/Pictures/soc-logo.png" alt="soc" />
+      </a>
     </footer>
   );
 }
--- goosfraba/src/Components/List-buttons/listButtons.js
+++ goosfraba/src/Components/List-buttons/listButtons.js
@@ -1,33 +1,35 @@
-// create list buttons functions
-// Need three buttons
-import "./listButtons.css"
+import './listButtons.css';
 
-/**
+/** ListButtons Docs
  * Imports needed: ./listButtons.css (style)
- * This function adds the functions to each buttons 
+ * This function adds the functions to each buttons
  * ALso adds the images to each button to have them instead of text.
  * This function takes the props of each item on the list as an object:
  * @param {object} props - the following props:
  * handleDeleteClick - function that updates the setState by deleting an item from the list finding it with its id
  * handleDoneClick - function that updates the setState by crossing an item as done from the list finding it with its id
  * id: number - index created when listDataState was mapped in App.js
- * to_do_id - NOT IN USE - ID of the To do Item 
+ * to_do_id - NOT IN USE - ID of the To do Item
  * @returns the buttons with their functionality of deleting and crossing out, also adds an image to each button
  */
-
 function ListButtons({ handleDeleteClick, to_do_id, handleDoneClick, id }) {
   return (
     <section>
-
-      <button id="Buttons" className="editB"><img id="img" src="/Pictures/editButton.png" alt="edit Button" /></button>
-      <button id="Buttons" className="tickB"
+      <button id="Buttons" className="editB">
+        <img id="img" src="/Pictures/editButton.png" alt="edit Button" />
+      </button>
+      <button
+        id="Buttons"
+        className="tickB"
         onClick={() => {
           handleDoneClick(id);
         }}
-      ><img id="img" src="/Pictures/tickButton.png" alt="done Button" />
-
+      >
+        <img id="img" src="/Pictures/tickButton.png" alt="done Button" />
       </button>
-      <button id="Buttons" className="delB"
+      <button
+        id="Buttons"
+        className="delB"
         onClick={() => {
           handleDeleteClick(id);
         }}
