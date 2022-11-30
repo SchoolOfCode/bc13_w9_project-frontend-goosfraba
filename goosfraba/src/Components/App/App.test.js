a06183217c8c68f26415e4e5a8d1362fd2ade29a
violetacf <violeta@cique.es>
2022-11-30 16:18:05 +0100

    tests frontend

--- goosfraba/src/Components/App/App.test.js
+++ goosfraba/src/Components/App/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Goosfraba home contains all the elements', () => {
+  test('renders Goosfraba home', () => {
+    render(<App />);
+    const title = screen.getByText(/G o o s f r a b a/i);
+    expect(title).toBeInTheDocument();
+  });
+
+//   test('there are seven pictures present', () => {
+//     render(<App />);
+//     const logos = screen.getByTestId('links-logo');
+//     expect(logos.length).toBe(7);
+//   });
 });
